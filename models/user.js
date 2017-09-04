c15b64b688089f00e267c18b57d00d5a46e64753
Kristofer Rye <kristofer.rye@gmail.com>
2017-09-04 15:56:34 -0500

    Also add googleId to the user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     username: DataTypes.STRING,
+    googleId: DataTypes.BIGINT,
     email: DataTypes.STRING
   }, {
 		freezeTableName: true
