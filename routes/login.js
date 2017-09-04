c4477897026eed811a518dab9dc6e1f10be99d7c
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-04 17:17:55 -0500

    Use node-fetch to make verification request with Google

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,8 @@ import CLIENT_ID from '../config'
 import express from 'express'
 import models from '../models'
 import qs from 'query-string'
-import verifySub from ''
+import verifySub from '../lib/verifySub'
+import fetch from 'node-fetch'
 
 const router = express.Router()
 
@@ -12,10 +13,16 @@ const getToken = (tokString) => {
 }
 
 router.post('/google', (req, res) => {
-		fetch('https://www.googleapis.com/oauth2/v3/' + qs.stringify({
-			id_token: getToken(req.get('Authorization'))
+		let token = getToken(req.get('Authorization'))
+		console.log('https://www.googleapis.com/oauth2/v3/tokeninfo?' + qs.stringify({
+			id_token: token
+		}))
+		fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?' + qs.stringify({
+			id_token: token
 		}))
 		.then((googleObj) => {
+			googleObj = googleObj.body
+			console.log('got a response from google')
 			// verify some stuff
 			if (googleObj.aud !== CLIENT_ID) {
 				// This is not a token for our app. Error!
@@ -28,10 +35,14 @@ router.post('/google', (req, res) => {
 				return
 			}
 			// Put the sub into the DB if it's not there, if it is, make sure it is the same
-			verifySub(googleObj.sub)
-
+			if (verifySub(googleObj.sub)) {
+				res.sendStatus(200)
+			} else {
+				res.sendStatus(403)
+			}
 		})
 		.error(() => {
+			console.log('error sending verification request to google')
 			res.sendStatus(500)
 		})
 	}
