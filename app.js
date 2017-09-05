e6e50932e18000b931a6040ad7249f51e49a7b2d
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-05 15:18:24 -0500

    Add middleware to get current election

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
-import passport from 'passport'
 import { CLIENT_ID, CLIENT_SECRET, SESSION_SECRET } from './config.js'
-import session from 'express-session'
+import models from './models'
+import getCurrentElectionId from './lib/getCurrentElectionId'
 
 const Strategy = require('passport-google-oauth').Strategy
 const elections = require('./routes/elections')
@@ -26,6 +26,21 @@ app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
 	res.setHeader('Access-Control-Allow-Credentials', true)
+
+// election middleware
+app.use((req, res, next) => {
+	if (req.body.electionId) {
+		return // there is a electionId in the body already, we don't need to add one
+	}
+	if (req.query.electionId) {
+		req.body.electionId = req.query.electionId
+		return
+	}
+	if (req.params.electionId) {
+		req.body.electionId = req.params.electionId
+		return
+	}
+	req.body.electionId = getCurrentElectionId()
 	next()
 })
 
