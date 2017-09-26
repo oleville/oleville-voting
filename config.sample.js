4ca528c42d59bb4f5a7c9ab25ea6522ef10109a7
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-26 13:53:57 -0500

    Add admin usergroup as config

--- config.sample.js
+++ config.sample.js
@@ -2,3 +2,4 @@ export const CLIENT_ID = ''
 export const CLIENT_SECRET = ''
 export const SESSION_SECRET = ''
 export const CURRENT_ELECTION_INFORMATION_TIMEOUT = 60
+export const ADMIN_USERGROUP_ID = 1
