2bc090ab9da1409f8a33887380e9fe18dc5ffee2
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-26 14:34:08 -0500

    Protect POST, PATCH, DELETE of /candidates

--- routes/candidates.js
+++ routes/candidates.js
@@ -16,56 +16,68 @@ router.get('/:candidateId?', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-	models.Candidate.create({
-		name: req.query.name,
-		description: req.query.description,
-		electionId: req.query.electionId,
-		positionId: req.query.positionId
-	})
-	.then(() => {
-		res.sendStatus(201) // Created
-	})
-	.error(() => {
-		res.sendStatus(500) // Internal server error
-	})
+	if (!userIsAdmin(req)) {
+		res.sendStatus(403)
+	} else {
+		models.Candidate.create({
+			name: req.query.name,
+			description: req.query.description,
+			electionId: req.query.electionId,
+			positionId: req.query.positionId
+		})
+		.then(() => {
+			res.sendStatus(201) // Created
+		})
+		.error(() => {
+			res.sendStatus(500) // Internal server error
+		})
+	}
 })
 
 router.patch('/:candidateId', (req, res) => {
-	models.Candidate.find({
-		where: {
-			id: req.params.candidateId
-		}
-	})
-	.then((candidate) => {
-		if (!candidate) {
-			res.sendStatus(404)
-			return
-		}
-		candidate.name = req.query.name || candidate.name
-		candidate.description = req.query.description || candidate.description
-		candidate.electionId = req.query.electionId || candidate.electionId
-		candidate.positionId = req.query.positionId || candidate.positionId
-		candidate.save().then(() => {
-			res.sendStatus(202)
+	if (!userIsAdmin(req)) {
+		res.sendStatus(403)
+	} else {
+		models.Candidate.find({
+			where: {
+				id: req.params.candidateId
+			}
 		})
-	})
-	.error(() => {
-		res.sendStatus(500)
-	})
+		.then((candidate) => {
+			if (!candidate) {
+				res.sendStatus(404)
+				return
+			}
+			candidate.name = req.query.name || candidate.name
+			candidate.description = req.query.description || candidate.description
+			candidate.electionId = req.query.electionId || candidate.electionId
+			candidate.positionId = req.query.positionId || candidate.positionId
+			candidate.save().then(() => {
+				res.sendStatus(202)
+			})
+		})
+		.error(() => {
+			res.sendStatus(500)
+		})
+	}
 })
 
 router.delete('/:candidateId', (req, res) => {
-	models.Candidate.destroy({
-		where: {
-			id: req.params.candidateId
-		}
-	})
-	.then(() => {
-		res.sendStatus(202)
-	})
-	.error(() => {
-		res.sendStatus(500)
-	})
+	if (!userIsAdmin(req)) {
+		res.sendStatus(403)
+	} else {
+		models.Candidate.destroy({
+			where: {
+				id: req.params.candidateId
+			}
+		})
+		.then(() => {
+			res.sendStatus(202)
+		})
+		.error(() => {
+			res.sendStatus(500)
+		})
+	}
 })
 
 module.exports = router
