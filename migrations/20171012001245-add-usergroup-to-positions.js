9bdf63e3cd89fb0f68d7096af3f66010385a2cc5
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-10-11 19:18:45 -0500

    Add FK to position

--- migrations/20171012001245-add-usergroup-to-positions.js
+++ migrations/20171012001245-add-usergroup-to-positions.js
@@ -3,9 +3,21 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
 		queryInterface.addColumn('Position', 'userGroupId', Sequelize.INTEGER)
+		queryInterface.addConstraint('Position', ['userGroupId'], {
+			type: 'FOREIGN KEY',
+		  name: 'position_userGroupId_FK',
+		  references: {
+				table: 'UserGroup',
+		    field: 'id'
+			},
+		  onDelete: 'CASCADE',
+		  onUpdate: 'CASCADE'
+			}
+		)
   },
 
   down: function (queryInterface, Sequelize) {
 		queryInterface.removeColumn('Position', 'userGroupId')
+		queryInterface.removeConstraint('Position', 'position_useGroupId_FK')
   }
 };
